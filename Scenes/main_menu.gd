5b2ceb1abfc3e580af1aeecfa630f44c678ce743
Fabio Niglio <fabio_niglio@hotmail.com>
2025-11-29 18:25:51 +0100

    Add game scene and note picker functionality with associated resources

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -13,6 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	print("start pressed")
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
 
 
 func _on_settings_pressed() -> void:
